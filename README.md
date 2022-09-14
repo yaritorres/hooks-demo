5943ec5db237d7f0c3097c67a5bc3d471f4d19ed
Yari Torres Nicola <ybtorres9@gmail.com>
2022-09-14 16:35:14 -0600

    hooks demo finished and read me updated with fancy formatting

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # hooks-demo
 A short demo on using React hooks and what they do as understood by a junior developer.
+
+# getting started
+```
+npm install
+```
+
+# running the app
+```
+npm run build
+npm start
+```
+
+The app should appear in localhost:3000.
--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -4,18 +4,20 @@ import Buttons from '../components/Buttons.jsx';
 const Title = (props) => {
   const [color, setColor] = useState('purple');
 
-  // useEffect(() => {
-  //   console.log('mounting');
+  useEffect(() => {
+    console.log('mounting');
 
-  //   setColor('green');
+    if (color === 'purple') {
+      setColor('orange');
+    }
 
-  //   return () => { console.log('unmounting'); }
-  // })
+    return () => { console.log('unmounting'); }
+  })
 
   return (
     <div id="title-div">
-      <h1 style={{color: color}} onClick={() => { setColor('purple') }}> This is a title. </h1>
-      <Buttons setColor={setColor} index={index} setIndex={setIndex} passedArr={colorArr[index]} colorArr={colorArr} />
+      <h1 style={{color: color}} onClick={() => { setColor('orange') }}> This is a title. </h1>
+      <Buttons setColor={setColor} />
     </div>
   )
 }
